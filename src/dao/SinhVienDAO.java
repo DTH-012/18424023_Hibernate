d5538e8a7ca72d43de45fac0d27aeadb9bd99553
htdat0202 <htdat0202@gmail.com>
2019-08-25 19:35:41 +0700

    get SinhVien

--- src/dao/SinhVienDAO.java
+++ src/dao/SinhVienDAO.java
@@ -14,4 +14,34 @@ import pojo.Sinhvien;
 import util.HibernateUtil;
 
 public class SinhVienDAO {
+    public static List<Sinhvien> layDanhSachSinhvien() {
+        List<Sinhvien> ds = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            String hql = "select sv from Sinhvien sv";
+            Query query = session.createQuery(hql);
+            ds = query.list();
+        } catch (HibernateException ex) {
+            //Log the exception
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+        return ds;
+    }
+    
+    public static Sinhvien layThongTinSinhvien(String maSinhvien) {
+        Sinhvien sv = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            sv = (Sinhvien) session.get(Sinhvien.class,
+            maSinhvien);
+        } catch (HibernateException ex) {
+            //Log the exception
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+        return sv;
+    }
 }
